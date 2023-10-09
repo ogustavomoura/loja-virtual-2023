49e08a7043f550170592ace094a214c25d2765e1
Gustavo Moura <soaaresmoura@gmail.com>
2023-10-09 16:51:49 -0300

    crud permissao_pessoa

--- backend/src/main/java/com/dev/backend/entity/Pessoa.java
+++ backend/src/main/java/com/dev/backend/entity/Pessoa.java
@@ -1,17 +1,22 @@
 package com.dev.backend.entity;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa")
@@ -26,6 +31,10 @@ public class Pessoa {
     @JoinColumn(name = "idCidade")
     private Cidade cidade;
 
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @Setter(value = AccessLevel.NONE)
+    private List<PermissaoPessoa> permissaoPessoas;
+
     private String nome;
     private String cpf;
     private String email;
@@ -37,5 +46,12 @@ public class Pessoa {
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
+
+    public void setPermissaoPessoas(List<PermissaoPessoa> pp){
+        for (PermissaoPessoa p:pp){
+            p.setPessoa(this);
+        }
+        this.permissaoPessoas = pp;
+    }
     
 }
